efa1bd4dafe8a32a9a698af26b5581e63ad5be3d
Mariano Guerra <mariano@marianoguerra.org>
2020-03-02 19:35:10 +0100

    improve helpers

--- helpers.js
+++ helpers.js
@@ -2,34 +2,69 @@
 /*globals Vue*/
 
 function main() {
-  new Vue({
+  const app = new Vue({
     el: '#app',
     data: {
       curAuthor: '',
+      curSlackThread: '',
+      url: '',
+      urlTitle: '',
+      lastSelectedEmoji: '',
       authors: {
         'Sol Bekic': '[Sol Bekic](https://twitter.com/S0lll0s)',
         'Shalabh Chaturvedi':
-          '[Shalabh Chaturvedi](https://mobile.twitter.com/chatur_shalabh)',
+          '[Shalabh Chaturvedi](https://twitter.com/chatur_shalabh)',
         'Geoffrey Litt': '[Geoffrey Litt](https://twitter.com/geoffreylitt)',
-        'Kartik Agaram': '[Kartik Agaram](http://akkartik.name/about)'
+        'Kartik Agaram': '[Kartik Agaram](http://akkartik.name/about)',
+        'Scott Anderson':
+          '[Scott Anderson](https://twitter.com/ImpossibleScott)',
+        'Will Crichton': '[Will Crichton](willcrichton.net)'
       }
     },
     computed: {
       curAuthorText: function() {
         return this.authors[this.curAuthor] || '**' + this.curAuthor + '**';
+      },
+      curSlackThreadText: function() {
+        return '(🧵[Slack Thread](' + this.curSlackThread + '))';
+      },
+      urlMd: function() {
+        return `[${this.urlTitle}](${this.url})`;
+      },
+      message: function() {
+        return (
+          this.lastSelectedEmoji +
+          ' ' +
+          this.urlMd +
+          '. Via ' +
+          this.curAuthorText +
+          ' ' +
+          this.curSlackThreadText
+        );
       }
     },
 
     methods: {
       copyAuthorText: function() {
         navigator.clipboard.writeText(this.curAuthorText);
+      },
+      copySlackThreadText: function() {
+        navigator.clipboard.writeText(this.curSlackThreadText);
+      },
+      copyMessage: function() {
+        navigator.clipboard.writeText(this.message);
+      },
+      copyUrl: function() {
+        navigator.clipboard.writeText(this.urlMd);
       }
     }
   });
-}
 
-window.copyInnerContent = function(e) {
-  navigator.clipboard.writeText(e.target.innerText);
-};
+  window.copyInnerContent = function(e) {
+    const emoji = e.target.innerText;
+    navigator.clipboard.writeText(emoji);
+    app.lastSelectedEmoji = emoji;
+  };
+}
 
 main();
