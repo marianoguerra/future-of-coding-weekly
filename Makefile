e167872c21a782a752ce2b39ac02ecc765863546
Mariano Guerra <mariano@marianoguerra.org>
2020-04-27 10:13:05 +0200

    archive last week

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+history-index:
+	cd history; ls -1 2020-*/*/*.json > index.txt
--- history/app.js
+++ history/app.js
@@ -84,6 +84,7 @@ const ENTITIES_TO_TEXT = {lt: '<', gt: '>', amp: '&'},
 	USER_REF_REGEX = /<@(.*?)>/g,
 	ENTITIES_REGEX = /&(gt|lt|amp);/g,
 	GROUP_REF_REGEX = /<#(.*?)\|(.*?)>/g,
+	URL_REF_REGEX = /<(.*?)\|(.*?)>/g,
 	THING_REF_REGEX = /<!(.*?)>/g,
 	SKIN_REF_REGEX_TEXT = ':(' + Object.keys(skinIdsToCodes).join('|') + '):',
 	SKIN_REF_REGEX = new RegExp(SKIN_REF_REGEX_TEXT, 'g'),
@@ -100,6 +101,10 @@ function enrichMessage(msg, users) {
 		.replace(
 			GROUP_REF_REGEX,
 			(_, _gid, groupName) => '**`#' + groupName + '`**'
+		)
+		.replace(
+			URL_REF_REGEX,
+			(_, url, label) => `[${label}](${url})`
 		)
 		.replace(
 			THING_REF_REGEX,
