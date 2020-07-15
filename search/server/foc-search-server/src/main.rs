499f8f666dd084548ff98eedf1c0131694e68828
Mariano Guerra <mariano@marianoguerra.org>
2020-07-15 22:57:05 +0200

    almost there with the api but canteen doesn't support query strings O.o

--- search/server/foc-search-server/src/main.rs
+++ search/server/foc-search-server/src/main.rs
@@ -1,40 +1,78 @@
 use sonic_client::search::SearchChan;
 
+use canteen::utils;
+use canteen::{Canteen, Method, Request, Response};
+
+fn search_handler(req: &Request) -> Response {
+    let mut res = Response::new();
+
+    let host = std::env::var("FOC_SONIC_HOST").unwrap_or("127.0.0.1".to_string());
+    let password = std::env::var("FOC_SONIC_PASSWORD").unwrap_or("SecretPassword".to_string());
+    let port = std::env::var("FOC_SONIC_PORT")
+        .unwrap_or("1491".to_string())
+        .parse::<usize>()
+        .expect("FOC_SONIC_PORT is not a number");
+
+    match search(
+        &host,
+        port,
+        &password,
+        "messages",
+        req.params.get("channel").unwrap_or(&"general".to_string()),
+        req.params
+            .get("query")
+            .unwrap_or(&"Bret Victor".to_string()),
+        Some(10000),
+        None,
+    ) {
+        Ok(items) => {
+            res.set_status(200);
+            res.set_content_type("application/json");
+            res.append(format!("{:?}", items));
+        }
+        Err(err) => {
+            println!("{:?}", err);
+            res.set_status(500);
+            res.set_content_type("application/json");
+            res.append("{\"ok\":false,\"reason\":\"unknown\"}");
+        }
+    }
+
+    res
+}
+
 fn main() {
-    let host = "127.0.0.1";
-    let port = 1491;
-    let password = "SecretPassword";
+    let mut cnt = Canteen::new();
+    let http_host = std::env::var("FOC_HTTP_HOST").unwrap_or("127.0.0.1".to_string());
+    let http_port = std::env::var("FOC_HTTP_PORT")
+        .unwrap_or("8080".to_string())
+        .parse::<u16>()
+        .expect("FOC_HTTP_PORT is not a number");
 
+    // bind to an address
+    cnt.bind((&*http_host, http_port));
+
+    // set the default route handler to show a 404 message
+    cnt.set_default(utils::err_404);
+
+    cnt.add_route("/search", &[Method::Get], search_handler);
+    cnt.add_route("/static/<path:path>", &[Method::Get], utils::static_file);
+
+    cnt.run();
+}
+
+fn search(
+    host: &str,
+    port: usize,
+    password: &str,
+    collection: &str,
+    channel: &str,
+    query: &str,
+    max_results: Option<i32>,
+    locale: Option<&str>,
+) -> Result<Vec<String>, std::io::Error> {
     let mut s = SearchChan::new(host, port, password).expect("Connection error");
-    let handle = s.read();
-    match s.connect() {
-        Ok(_) => {
-            let collection = "messages";
-            let channel = "general";
-            let query = "victor";
-            let max_results = 10000;
-            let offset = None;
-
-            match s.query(collection, channel, query, Some(max_results), offset) {
-                Ok(r) => {
-                    println!("result: {:?}", r);
-                }
-                Err(err) => {
-                    println!("Error in query: {:?}", err);
-                }
-            }
-        }
-        Err(err) => {
-            println!("Error in connect: {:?}", err);
-        }
-    }
-
-    match handle.join() {
-        Ok(_) => {
-            println!("join!");
-        }
-        Err(err) => {
-            println!("Error in join: {:?}", err);
-        }
-    }
+    s.read();
+    s.connect()?;
+    s.query(collection, channel, query, max_results, locale)
 }
