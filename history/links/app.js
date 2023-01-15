45d1d6f6e2ce3910c234f3afdcafd1448c17956c
Mariano Guerra <mariano@marianoguerra.org>
2023-01-15 22:18:27 +0100

    better link load and filter

--- history/links/app.js
+++ history/links/app.js
@@ -1,15 +1,45 @@
 //@format
-/*globals Vue*/
+/*globals Vue, TextDecoderStream*/
 function main() {
+  let nextFilterId = null;
   const app = new Vue({
     el: '#app',
-    data: {links: [], allLinks: [], filter: ''},
+    data: {links: [], allLinks: [], filter: '', loading: false},
     methods: {
       refilter: function () {
-        const filter = this.filter.toLowerCase();
-        this.links = this.allLinks.filter(({_searchText}) =>
-          _searchText.includes(filter)
-        );
+        const filter = this.filter.toLowerCase().trim();
+        this.links = [];
+        if (filter === '') {
+          this.links = this.allLinks;
+        } else {
+          this.loading = true;
+          if (nextFilterId) {
+            clearTimeout(nextFilterId);
+            nextFilterId = null;
+          }
+          this._refilter(filter, this.allLinks, 0, 1000);
+        }
+      },
+      _refilter(filter, allLinks, i0, chunkSize) {
+        const len = Math.min(i0 + chunkSize, allLinks.length),
+          toAdd = [];
+        for (let i = i0; i < len; i++) {
+          const link = allLinks[i];
+          if (link._searchText.includes(filter)) {
+            toAdd.push(link);
+          }
+        }
+
+        this.links = this.links.concat(toAdd);
+        if (len < allLinks.length) {
+          nextFilterId = setTimeout(
+            () => this._refilter(filter, allLinks, len, chunkSize),
+            0
+          );
+        } else {
+          nextFilterId = null;
+          this.loading = false;
+        }
       },
       doSearch() {
         this.refilter();
@@ -17,34 +47,67 @@ function main() {
     },
   });
 
-  fetch('./links.txt')
-    .then((res) => res.text())
-    .then((text) => {
-      for (let line of text.split('\n')) {
-        if (!line) {
-          continue;
+  async function fetchUrlChunks(url, handleChunk, onFinish) {
+    const response = await fetch(url);
+    const reader = response.body
+      .pipeThrough(new TextDecoderStream())
+      .getReader();
+    fetchChunks(reader, '', handleChunk, onFinish);
+  }
+
+  async function fetchChunks(reader, remaining, handleChunk, onFinish) {
+    const {value, done} = await reader.read(),
+      newRemaining = value ? handleChunk(remaining + value) : '';
+
+    if (done) {
+      onFinish();
+    } else {
+      window.setTimeout(
+        () => fetchChunks(reader, newRemaining, handleChunk, onFinish),
+        0
+      );
+    }
+  }
+
+  function handleUrlsChunk(data) {
+    let remaining = '';
+    if (data) {
+      data.split(/\n/g).forEach((line, _i, _) => {
+        if (line) {
+          try {
+            const [count, url, title, ts, status] = JSON.parse(line),
+              date = new Date(ts).toISOString().slice(0, 16).replace('T', ' '),
+              _searchText = `${count}\n${url}\n${title.toLowerCase()}\n${date}\n${status}`,
+              isOkLink = status >= 200 && status <= 299,
+              linkStatus = isOkLink ? 'ok' : 'error';
+
+            app.allLinks.push({
+              count,
+              url,
+              title,
+              status,
+              linkStatus,
+              ts,
+              date,
+              _searchText,
+            });
+          } catch (err) {
+            console.error(line);
+            remaining = line;
+          }
         }
+      });
+    }
+    return remaining;
+  }
 
-        const [count, url, title, ts, status] = JSON.parse(line),
-          date = new Date(ts).toISOString().slice(0, 16).replace('T', ' '),
-          _searchText = `${count}\n${url}\n${title.toLowerCase()}\n${date}\n${status}`,
-          isOkLink = status >= 200 && status <= 299,
-          linkStatus = isOkLink ? 'ok' : 'error';
+  app.loading = true;
+  function onUrlsFinish() {
+    app.loading = false;
+    app.refilter();
+  }
 
-        app.allLinks.push({
-          count,
-          url,
-          title,
-          status,
-          linkStatus,
-          ts,
-          date,
-          _searchText,
-        });
-      }
-
-      app.refilter();
-    });
+  fetchUrlChunks('./links.txt', handleUrlsChunk, onUrlsFinish);
 }
 
 main();
