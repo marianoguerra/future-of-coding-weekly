32f9aa1c0cf01b4545d010112962e8a0950fb37b
Mariano Guerra <mariano@marianoguerra.org>
2023-05-14 23:01:10 +0200

    ch ch changes

--- app.js
+++ app.js
@@ -28,16 +28,16 @@ function ce(tag, attrs, ...body) {
 
 let mdOutput = "";
 function handleComment(comment, node, authors) {
-  const container = ce('div', {style: 'margin-top:1em'}),
+  const container = ce("div", { style: "margin-top:1em" }),
     markdown = comment.body,
     matchResult = (
       new RegExp(
-        '\\[([^\\[]*?)\\]\\((https?://(www\\.)?twitter.com/([^/]+))\\)',
-        'gm'
+        "\\[([^\\[]*?)\\]\\((https?://(www\\.)?twitter.com/([^/]+))\\)",
+        "gm",
       ).exec(markdown) || []
     ).slice(1);
 
-  mdOutput += comment.body + '\n\n';
+  mdOutput += comment.body + "\n\n";
   container.innerHTML = mdToHTML(markdown);
 
   matchResult.forEach((v, i, _arr) => {
@@ -48,96 +48,99 @@ function handleComment(comment, node, authors) {
   });
 
   node.appendChild(container);
-  node.appendChild(ce('br', {}));
+  node.appendChild(ce("br", {}));
   // The beforebegin and afterend positions work only if the node is in a tree
   // and has an element parent.
-  container.insertAdjacentText('beforebegin', '\n\n');
-  container.insertAdjacentText('afterend', '\n\n');
+  container.insertAdjacentText("beforebegin", "\n\n");
+  container.insertAdjacentText("afterend", "\n\n");
 }
 
 function link(url, label) {
-  return ce('a', {href: url, target: '_blank'}, label || url);
+  return ce("a", { href: url, target: "_blank" }, label || url);
 }
 
-const TWITTER_HANDLES_TO_FILTER = new Set(['warianoguerra']);
+const TWITTER_HANDLES_TO_FILTER = new Set(["warianoguerra"]);
 function onCommentsFinished(_contributors, authors) {
   console.log(authors);
-  const outputNode = document.getElementById('output'),
+  const outputNode = document.getElementById("output"),
     authorsSorted = Array.from(authors).sort((userA, userB) =>
       userA.localeCompare(userB)
     );
 
   outputNode.appendChild(
-    ce('hr', {style: 'border:0;border-top:1px solid #cccccc'})
+    ce("hr", { style: "border:0;border-top:1px solid #cccccc" }),
   );
 
   window.setTimeout(() => {
     const twitterHandles = authorsSorted
         .filter((name) => !TWITTER_HANDLES_TO_FILTER.has(name))
-        .map((name) => '@' + name)
-        .join(' '),
-      textArea = document.getElementById('output-help'),
-      textAreaMd = document.getElementById('output-md'),
-      {newsletterUrl} = getInfoForWeekAndDay(new Date(), MONDAY);
+        .map((name) => "@" + name)
+        .join(" "),
+      textArea = document.getElementById("output-help"),
+      textAreaMd = document.getElementById("output-md"),
+      { newsletterUrl } = getInfoForWeekAndDay(new Date(), MONDAY);
     console.log(twitterHandles);
 
     textArea.innerHTML = textArea.innerHTML
-      .replace('%NEWSLETTER_URL%', newsletterUrl)
-      .replace('%TWITTER_HANDLES%', twitterHandles)
-      .replace(/%TOPICS%/g, textAreaMd.innerHTML.trim().split('\n')[0]);
+      .replace("%NEWSLETTER_URL%", newsletterUrl)
+      .replace("%TWITTER_HANDLES%", twitterHandles)
+      .replace(/%TOPICS%/g, textAreaMd.innerHTML.trim().split("\n")[0]);
   }, 500);
 
   outputNode.appendChild(
     ce(
-      'p',
+      "p",
       {},
-      '👨🏽‍💻 By ',
-      link('https://vis.social/@marianoguerra', '🐘 @marianoguerra@vis.social'),
-      ' ',
-      link('https://twitter.com/warianoguerra', '🐦 @warianoguerra')
-    )
-  );
-
-  outputNode.appendChild(
-    ce(
-      'p',
-      {},
-      '💬 Not a member yet? Check the ',
-      link('https://futureofcoding.org/', 'Future of Coding Community')
-    )
-  );
-
-  outputNode.appendChild(
-    ce(
-      'p',
-      {},
-      '✉️ Not subscribed yet? ',
+      "👨🏽‍💻 By ",
       link(
-        'https://tinyletter.com/marianoguerra/',
-        'Subscribe to the Newsletter'
+        "https://hachyderm.io/@marianoguerra",
+        "🐘 @marianoguerra@hachyderm.io",
       ),
-      ' / ',
-      link('https://tinyletter.com/marianoguerra/archive', 'Archive'),
-      ' / ',
-      link('https://history.futureofcoding.org/newsletter/rss.xml', 'RSS')
-    )
+      " ",
+      link("https://twitter.com/warianoguerra", "🐦 @warianoguerra"),
+    ),
   );
 
   outputNode.appendChild(
     ce(
-      'p',
+      "p",
       {},
-      '🎙 Prefer podcasts? check the ',
-      link('https://futureofcoding.org/episodes/', 'Future of Coding Podcast')
-    )
+      "💬 Not a member yet? Check the ",
+      link("https://futureofcoding.org/", "Future of Coding Community"),
+    ),
   );
 
-  outputNode.appendChild(ce('p', {}, ''));
+  outputNode.appendChild(
+    ce(
+      "p",
+      {},
+      "✉️ Not subscribed yet? ",
+      link(
+        "https://tinyletter.com/marianoguerra/",
+        "Subscribe to the Newsletter",
+      ),
+      " / ",
+      link("https://tinyletter.com/marianoguerra/archive", "Archive"),
+      " / ",
+      link("https://history.futureofcoding.org/newsletter/rss.xml", "RSS"),
+    ),
+  );
 
-  document.getElementById('output-html').textContent = outputNode.innerHTML
-    .replace(/<p>/g, '\n\n<p>')
+  outputNode.appendChild(
+    ce(
+      "p",
+      {},
+      "🎙 Prefer podcasts? check the ",
+      link("https://futureofcoding.org/episodes/", "Future of Coding Podcast"),
+    ),
+  );
+
+  outputNode.appendChild(ce("p", {}, ""));
+
+  document.getElementById("output-html").textContent = outputNode.innerHTML
+    .replace(/<p>/g, "\n\n<p>")
     .trim();
-  document.getElementById('output-md').textContent = mdOutput;
+  document.getElementById("output-md").textContent = mdOutput;
 }
 
 function addCommentSeparator(_outputNode) {
@@ -145,7 +148,7 @@ function addCommentSeparator(_outputNode) {
 }
 
 function onComments(comments, baseUrl, count, contributors, authors) {
-  const outputNode = document.getElementById('output'),
+  const outputNode = document.getElementById("output"),
     lastIndex = comments.length - 1;
 
   comments.forEach((comment, i, _it) => {
@@ -164,8 +167,9 @@ function onComments(comments, baseUrl, count, contributors, authors) {
 }
 
 function loadCommentsPage(baseUrl, count, contributors, authors, callback) {
-  fetchJson(baseUrl + count, (comments) =>
-    callback(comments, baseUrl, count + 1, contributors, authors)
+  fetchJson(
+    baseUrl + count,
+    (comments) => callback(comments, baseUrl, count + 1, contributors, authors),
   );
 }
 
@@ -173,35 +177,35 @@ function handleIssue(issue) {
   const contributors = {},
     authors = new Set();
   loadCommentsPage(
-    issue.comments_url + '?page=',
+    issue.comments_url + "?page=",
     1,
     contributors,
     authors,
-    onComments
+    onComments,
   );
 }
 
 function showMsg(className, txt) {
-  const node = document.getElementById('msg');
+  const node = document.getElementById("msg");
   node.className = className;
-  node.style.display = 'block';
+  node.style.display = "block";
   node.innerText = txt;
 }
 
 function noIssuesOpen() {
-  showMsg('alert alert-danger', 'No Issues Open');
+  showMsg("alert alert-danger", "No Issues Open");
 }
 
 function moreThanOneIssueOpen() {
   showMsg(
-    'alert alert-warning',
-    'More than one issue open, picking latest one'
+    "alert alert-warning",
+    "More than one issue open, picking latest one",
   );
 }
 
 function handleIssues(data) {
   const openIssues = data
-    .filter((issue) => issue.state === 'open')
+    .filter((issue) => issue.state === "open")
     .sort((issueA, issueB) => issueA.number - issueB.number);
 
   if (openIssues.length === 0) {
@@ -222,37 +226,37 @@ function fetchJson(url, onData) {
 }
 
 function main() {
-  console.log('fetching issues');
+  console.log("fetching issues");
   fetchJson(ISSUES_URL, handleIssues);
 }
 
 const defaultRules = SimpleMarkdown.defaultRules;
 
 function overrideDefaultHtml(ruleName, overrideFn) {
-  return Object.assign({}, defaultRules[ruleName], {html: overrideFn});
+  return Object.assign({}, defaultRules[ruleName], { html: overrideFn });
 }
 
 const customRules = {
-    paragraph: overrideDefaultHtml('paragraph', function (node, output, state) {
-      return '<p>' + output(node.content, state) + '</p>\n';
+    paragraph: overrideDefaultHtml("paragraph", function (node, output, state) {
+      return "<p>" + output(node.content, state) + "</p>\n";
     }),
     blockQuote: overrideDefaultHtml(
-      'blockQuote',
+      "blockQuote",
       function (node, output, state) {
         return (
           '<blockquote style="margin-left:1em;color:#555555;font-style:italic">' +
           output(node.content, state) +
-          '</blockquote>\n'
+          "</blockquote>\n"
         );
-      }
+      },
     ),
   },
   rules = Object.assign({}, defaultRules, customRules),
   rawBuiltParser = SimpleMarkdown.parserFor(rules),
-  htmlOutput = SimpleMarkdown.htmlFor(SimpleMarkdown.ruleOutput(rules, 'html'));
+  htmlOutput = SimpleMarkdown.htmlFor(SimpleMarkdown.ruleOutput(rules, "html"));
 
 function mdToHTML(txt) {
-  return htmlOutput(rawBuiltParser(txt + '\n\n', {inline: false}));
+  return htmlOutput(rawBuiltParser(txt + "\n\n", { inline: false }));
 }
 
 main();
--- remindeme.js
+++ remindeme.js
@@ -1,4 +1,4 @@
-import {getInfoForWeekAndDay, MONDAY} from './history/newsletter.js';
+import { getInfoForWeekAndDay, MONDAY } from "./history/newsletter.js";
 
 function generateScriptForThisWeek(now, dayNumber) {
   const {
@@ -12,22 +12,18 @@ function generateScriptForThisWeek(now, dayNumber) {
     newsletterUrl,
   } = getInfoForWeekAndDay(now, dayNumber);
 
+  console.log("bash");
+  console.log("source ./bin/activate");
+  console.log("nu");
   console.log(`focWeekExport "${prevDateStr}" "${nextDateStr}"`);
   console.log(
-    `https://github.com/marianoguerra/future-of-coding-weekly/issues/new?&template=newsletter-week-issue.md&title=Future+of+Coding+Weekly+${curYear}%2F${monthStr}+Week+${weekNumber}`
+    `https://github.com/marianoguerra/future-of-coding-weekly/issues/new?&template=newsletter-week-issue.md&title=Future+of+Coding+Weekly+${curYear}%2F${monthStr}+Week+${weekNumber}`,
   );
-  console.log(issueTitle);
   console.log(`mkdir history/weekly/${curYear}/${monthStr}/${weekStr}/`);
   console.log(
-    `nix-shell`
+    `nix-shell;\ncd newsletter-content;nikola new_post -f markdown -t "${issueTitle}" -e`,
   );
-  console.log(
-    `cd newsletter-content;nikola new_post -f markdown -t "${issueTitle}" -e`
-  );
-  console.log(
-    `hx posts/future-of-coding-weekly-${curYear}${monthStr}-week-${weekNumber}.md`
-  );
-  console.log("nikola build");
+  console.log("nikola clean; nikola build; nikola serve");
   console.log(newsletterUrl);
 }
 
