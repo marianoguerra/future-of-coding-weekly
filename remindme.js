d3a23af96502d1d19ea897e9a6899efcc6de0b68
Mariano Guerra <mariano@marianoguerra.org>
2024-02-04 23:35:16 +0100

    better reminder

--- remindme.js
+++ remindme.js
@@ -2,27 +2,52 @@ import { getInfoForWeekAndDay, MONDAY } from "./history/newsletter.js";
 
 function generateScriptForThisWeek(now, dayNumber) {
   const {
-    prevDateStr,
-    nextDateStr,
-    weekStr,
-    weekNumber,
-    monthStr,
-    curYear,
-    issueTitle,
-    newsletterUrl,
-  } = getInfoForWeekAndDay(now, dayNumber);
+      prevDateStr,
+      nextDateStr,
+      weekStr,
+      weekNumber,
+      monthStr,
+      curYear,
+      issueTitle,
+      newsletterUrl,
+    } = getInfoForWeekAndDay(now, dayNumber),
+    dateDir = `${curYear}/${monthStr}/${weekStr}`;
 
   console.log("nix-shell");
-  console.log(`mkdir history/weekly/${curYear}/${monthStr}/${weekStr}/`);
+  console.log(`mkdir history/weekly/${dateDir}/`);
+  console.log(
+    `mkdir future-of-newsletters/mail-sent/${dateDir}/`,
+  );
+  console.log(
+    `sed 's/title = "Future of Coding Weekly"/title = "Future of Coding Weekly ${curYear}\\/${monthStr} Week ${weekNumber}"/g' future-of-newsletters/resources/mail-send-config.toml |\n save future-of-newsletters/mail-sent/${dateDir}/config.toml`,
+  );
+  console.log(
+    `bat future-of-newsletters/mail-sent/${dateDir}/config.toml`,
+  );
   console.log(`focWeekExport "${prevDateStr}" "${nextDateStr}"`);
   console.log(
     `https://github.com/marianoguerra/future-of-coding-weekly/issues/new?&template=newsletter-week-issue.md&title=Future+of+Coding+Weekly+${curYear}%2F${monthStr}+Week+${weekNumber}`,
+  );
+  console.log(`focAfterWeeklyPush`);
+  console.log(
+    `hx future-of-newsletters/mail-sent/${dateDir}/mail.html`,
+  );
+  console.log(
+    `hx future-of-newsletters/mail-sent/${dateDir}/mail.txt`,
   );
   console.log(
     `cd newsletter-content;nikola new_post -f markdown -t "${issueTitle}" -e`,
   );
   console.log("nikola clean; nikola build; nikola serve");
+  console.log("git add newsletter*");
+  console.log("git commit -m 'Newsletter and Links'");
+  console.log("git push origin");
+  console.log("focRemotePull");
   console.log(newsletterUrl);
+  console.log("# Don't run inside a nix-shell");
+  console.log(
+    `cd future-of-newsletters/foc-server/;cargo run -- send-newsletter --mail-path ../mail-sent/${dateDir}/ --db-path ~/.foc/foc.db`,
+  );
 }
 function dateStrAddDays(days) {
   const d = new Date();
