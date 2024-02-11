04f769bfc9abddc0d41bb61510e28d799cf0c146
Mariano Guerra <mariano@marianoguerra.org>
2024-02-12 00:16:18 +0100

    remind me better

--- remindme.js
+++ remindme.js
@@ -1,4 +1,4 @@
-import { getInfoForWeekAndDay, MONDAY } from "./history/newsletter.js";
+import {getInfoForWeekAndDay, MONDAY} from './history/newsletter.js';
 
 function generateScriptForThisWeek(now, dayNumber) {
   const {
@@ -13,49 +13,41 @@ function generateScriptForThisWeek(now, dayNumber) {
     } = getInfoForWeekAndDay(now, dayNumber),
     dateDir = `${curYear}/${monthStr}/${weekStr}`;
 
-  console.log("nix-shell");
+  console.log('nix-shell');
   console.log(`mkdir history/weekly/${dateDir}/`);
+  console.log(`mkdir future-of-newsletters/mail-sent/${dateDir}/`);
   console.log(
-    `mkdir future-of-newsletters/mail-sent/${dateDir}/`,
+    `sed 's/title = "Future of Coding Weekly"/title = "Future of Coding Weekly ${curYear}\\/${monthStr} Week ${weekNumber}"/g' future-of-newsletters/resources/mail-send-config.toml |\n save future-of-newsletters/mail-sent/${dateDir}/config.toml`
   );
-  console.log(
-    `sed 's/title = "Future of Coding Weekly"/title = "Future of Coding Weekly ${curYear}\\/${monthStr} Week ${weekNumber}"/g' future-of-newsletters/resources/mail-send-config.toml |\n save future-of-newsletters/mail-sent/${dateDir}/config.toml`,
-  );
-  console.log(
-    `bat future-of-newsletters/mail-sent/${dateDir}/config.toml`,
-  );
+  console.log(`bat future-of-newsletters/mail-sent/${dateDir}/config.toml`);
   console.log(`focWeekExport "${prevDateStr}" "${nextDateStr}"`);
   console.log(
-    `https://github.com/marianoguerra/future-of-coding-weekly/issues/new?&template=newsletter-week-issue.md&title=Future+of+Coding+Weekly+${curYear}%2F${monthStr}+Week+${weekNumber}`,
+    `https://github.com/marianoguerra/future-of-coding-weekly/issues/new?&template=newsletter-week-issue.md&title=Future+of+Coding+Weekly+${curYear}%2F${monthStr}+Week+${weekNumber}`
   );
   console.log(`focAfterWeeklyPush`);
+  console.log(`hx future-of-newsletters/mail-sent/${dateDir}/mail.html`);
+  console.log(`hx future-of-newsletters/mail-sent/${dateDir}/mail.txt`);
   console.log(
-    `hx future-of-newsletters/mail-sent/${dateDir}/mail.html`,
+    `cd newsletter-content;nikola new_post -f markdown -t "${issueTitle}" -e`
   );
-  console.log(
-    `hx future-of-newsletters/mail-sent/${dateDir}/mail.txt`,
-  );
-  console.log(
-    `cd newsletter-content;nikola new_post -f markdown -t "${issueTitle}" -e`,
-  );
-  console.log("nikola clean; nikola build; nikola serve");
-  console.log("git add newsletter*");
+  console.log('nikola clean; nikola build; nikola serve');
+  console.log('git add newsletter*');
   console.log(`git add future-of-newsletters/mail-sent/${dateDir}/`);
   console.log("git commit -m 'Newsletter and Links'");
-  console.log("git push origin");
-  console.log("focRemotePull");
+  console.log('git push origin');
+  console.log('focRemotePull');
   console.log(newsletterUrl);
   console.log("# Don't run inside a nix-shell");
   console.log(
-    `cd future-of-newsletters/foc-server/;cargo run -- send-newsletter --mail-path ../mail-sent/${dateDir}/ --db-path ~/.foc/foc.db`,
+    `cd future-of-newsletters/foc-server/;cargo run -- send-newsletter --config-path ./resources/base-config.toml --mail-path ../mail-sent/${dateDir}/ --db-path ~/.foc/foc.db`
   );
 }
 function dateStrAddDays(days) {
   const d = new Date();
   d.setUTCDate(d.getUTCDate() + days);
-  return d.toISOString().split("T")[0];
+  return d.toISOString().split('T')[0];
 }
 generateScriptForThisWeek(
   new Date(Date.parse(Deno.args[0] ?? dateStrAddDays(-1))),
-  MONDAY,
+  MONDAY
 );
