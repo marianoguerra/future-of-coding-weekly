034bce4d380c82d8f2d1d9208c164907e2847fea
Mariano Guerra <mariano@marianoguerra.org>
2024-12-01 23:37:59 +0100

    remind me better

--- remindme.js
+++ remindme.js
@@ -13,7 +13,6 @@ function generateScriptForThisWeek(now, dayNumber) {
     } = getInfoForWeekAndDay(now, dayNumber),
     dateDir = `${curYear}/${monthStr}/${weekStr}`;
 
-  console.log('nix-shell');
   console.log(`mkdir history/weekly/${dateDir}/`);
   console.log(`mkdir future-of-newsletters/mail-sent/${dateDir}/`);
   console.log(
@@ -21,24 +20,24 @@ function generateScriptForThisWeek(now, dayNumber) {
   );
   console.log(`bat future-of-newsletters/mail-sent/${dateDir}/config.toml`);
   console.log(`focWeekExport "${prevDateStr}" "${nextDateStr}"`);
-  console.log(`deno run remindme.js`);
   console.log(
     `https://github.com/marianoguerra/future-of-coding-weekly/issues/new?&template=newsletter-week-issue.md&title=Future+of+Coding+Weekly+${curYear}%2F${monthStr}+Week+${weekNumber}`
   );
   console.log(`git add history; git commit -m "weekly dump"; git push origin`);
   console.log(`focAfterWeeklyPush`);
-  console.log(`hx future-of-newsletters/mail-sent/${dateDir}/mail.html`);
-  console.log(`hx future-of-newsletters/mail-sent/${dateDir}/mail.txt`);
+  console.log(`vim future-of-newsletters/mail-sent/${dateDir}/mail.html`);
+  console.log(`vim future-of-newsletters/mail-sent/${dateDir}/mail.txt`);
   console.log(
     `cd newsletter-content;nikola new_post -f markdown -t "${issueTitle}" -e`
   );
   console.log('nikola clean; nikola build; nikola serve');
+  console.log('cd ..');
   console.log('git add newsletter*');
   console.log(`git add future-of-newsletters/mail-sent/${dateDir}/`);
   console.log("git commit -m 'Newsletter and Links'");
   console.log('git push origin');
   console.log('focRemotePull');
-  console.log(newsletterUrl);
+  console.log('firefox ' + newsletterUrl);
   console.log("# Don't run inside a nix-shell");
   console.log(
     `cd future-of-newsletters/foc-server/;cargo run -- send-newsletter --config-path ./resources/base-config.toml --mail-path ../mail-sent/${dateDir}/ --db-path ~/.foc/justmariano.db`
